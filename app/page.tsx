8d5ec8125ca22d16dbcd693b20aef318684334c7
amanzeao7 <amanzeao7@gmail.com>
2026-07-06 09:47:34 +0100

    Remove FightsSection and VideoSection imports
    Removed FightsSection and VideoSection from HomePage.

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import HeroSection from '@/components/home/HeroSection'
 import ClassesSection from '@/components/home/ClassesSection'
 import ScheduleSection from '@/components/home/ScheduleSection'
 import WhySection from '@/components/home/WhySection'
-import FightsSection from '@/components/home/FightsSection'
 import PricingSection from '@/components/home/PricingSection'
 import ContactSection from '@/components/home/ContactSection'
-import VideoSection from '@/components/fights/VideoSection'
-import { videoEmbeds } from '@/lib/data'
 
 export default function HomePage() {
   return (
@@ -15,8 +12,6 @@ export default function HomePage() {
       <ClassesSection />
       <ScheduleSection />
       <WhySection />
-      <FightsSection />
-      <VideoSection embeds={videoEmbeds} />
       <PricingSection />
       <ContactSection />
     </main>
